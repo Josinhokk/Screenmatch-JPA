6664a58f436bb4f88bf3f38c3985fb22f4435f8b
Kayke <kaykeflausino@hotmail.com>
2025-10-09 18:18:22 -0300

    COnectando a Web

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplicationSemWEb.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatch;
+
+//@SpringBootApplication
+//public class ScreenmatchApplicationse implements CommandLineRunner {
+//
+//	@Autowired
+//	private SerieRepository repositorio;
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchApplication.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Principal principal = new Principal(repositorio);
+//		principal.exibeMenu();
+//	}
+//}
